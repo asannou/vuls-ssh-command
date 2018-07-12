432fa44792a7dd07bf4c2bdeb721d070762d0635
asannou <hataori@gmail.com>
2018-07-12 18:59:31 +0900

    refactoring

--- vuls-ssh-command.sh
+++ vuls-ssh-command.sh
@@ -22,21 +22,27 @@
 
 # stty cols 1000; docker ps --format '{{.ID}} {{.Names}} {{.Image}}'
 # stty cols 1000; docker ps --filter 'status=exited' --format '{{.ID}} {{.Names}} {{.Image}}'
-# stty cols 1000; docker exec --user 0 08f3a0611cee /bin/sh -c 'ls /etc/debian_version'
-# stty cols 1000; docker exec --user 0 08f3a0611cee /bin/sh -c 'cat /etc/issue'
-# stty cols 1000; docker exec --user 0 08f3a0611cee /bin/sh -c 'lsb_release -ir'
-# stty cols 1000; docker exec --user 0 08f3a0611cee /bin/sh -c 'cat /etc/lsb-release'
-# stty cols 1000; docker exec --user 0 08f3a0611cee /bin/sh -c 'cat /etc/debian_version'
-# stty cols 1000; docker exec --user 0 08f3a0611cee /bin/sh -c 'type curl'
-# stty cols 1000; docker exec --user 0 08f3a0611cee /bin/sh -c 'type wget'
-# stty cols 1000; docker exec --user 0 08f3a0611cee /bin/sh -c '/sbin/ip -o addr'
-# stty cols 1000; docker exec --user 0 08f3a0611cee /bin/sh -c 'uname -r'
-# stty cols 1000; docker exec --user 0 08f3a0611cee /bin/sh -c 'uname -a'
-# stty cols 1000; docker exec --user 0 08f3a0611cee /bin/sh -c 'test -f /var/run/reboot-required'
-# stty cols 1000; docker exec --user 0 08f3a0611cee /bin/sh -c 'dpkg-query -W -f="\${binary:Package},\${db:Status-Abbrev},\${Version},\${Source},\${source:Version}\n"'
-# stty cols 1000; docker exec --user 0 08f3a0611cee /bin/sh -c 'apt-get update'
-# stty cols 1000; docker exec --user 0 08f3a0611cee /bin/sh -c 'LANGUAGE=en_US.UTF-8 apt-get dist-upgrade --dry-run'
-# stty cols 1000; docker exec --user 0 08f3a0611cee /bin/sh -c 'LANGUAGE=en_US.UTF-8 apt-cache policy '
+# stty cols 1000; docker exec --user 0 deadbeefdead /bin/sh -c 'ls /etc/debian_version'
+# stty cols 1000; docker exec --user 0 deadbeefdead /bin/sh -c 'cat /etc/issue'
+# stty cols 1000; docker exec --user 0 deadbeefdead /bin/sh -c 'lsb_release -ir'
+# stty cols 1000; docker exec --user 0 deadbeefdead /bin/sh -c 'cat /etc/lsb-release'
+# stty cols 1000; docker exec --user 0 deadbeefdead /bin/sh -c 'cat /etc/debian_version'
+# stty cols 1000; docker exec --user 0 deadbeefdead /bin/sh -c 'type curl'
+# stty cols 1000; docker exec --user 0 deadbeefdead /bin/sh -c 'type wget'
+# stty cols 1000; docker exec --user 0 deadbeefdead /bin/sh -c '/sbin/ip -o addr'
+# stty cols 1000; docker exec --user 0 deadbeefdead /bin/sh -c 'uname -r'
+# stty cols 1000; docker exec --user 0 deadbeefdead /bin/sh -c 'uname -a'
+# stty cols 1000; docker exec --user 0 deadbeefdead /bin/sh -c 'test -f /var/run/reboot-required'
+# stty cols 1000; docker exec --user 0 deadbeefdead /bin/sh -c 'dpkg-query -W -f="\${binary:Package},\${db:Status-Abbrev},\${Version},\${Source},\${source:Version}\n"'
+# stty cols 1000; docker exec --user 0 deadbeefdead /bin/sh -c 'apt-get update'
+# stty cols 1000; docker exec --user 0 deadbeefdead /bin/sh -c 'LANGUAGE=en_US.UTF-8 apt-get dist-upgrade --dry-run'
+# stty cols 1000; docker exec --user 0 deadbeefdead /bin/sh -c 'LANGUAGE=en_US.UTF-8 apt-cache policy '
+
+test_stty() {
+  IFS=' '
+  set -- $1
+  test "$1" = "stty" -a "$2" = "cols"
+}
 
 escape() {
   tr '\n' '\0' | sed 's/\x0/\\n/g;s/\t/\\t/g'
@@ -48,19 +54,34 @@ exec_command() {
   exec "$@"
 }
 
-verify_command() {
+verify_enviroment() {
   case "$1" in
     LANGUAGE=*)
-      shift
-      export LANGUAGE=en_US.UTF-8
+      echo "$1"
       ;;
   esac
+}
+
+verify_command() {
   case "$1" in
-    ls|cat|type|/sbin/ip|uname|test|repoquery|yum|dpkg-query)
+    ls|type|lsb_release|uname|test|repoquery|dpkg-query|apt-cache)
       echo "$@"
+      ;;
+    cat)
+      case "$2" in
+        /etc/system-release|/etc/issue|/etc/lsb-release|/etc/debian_version)
+          echo "$@"
+          ;;
+      esac
       ;;
     curl)
       echo curl --max-time 1 --retry 3 --noproxy 169.254.169.254 http://169.254.169.254/latest/meta-data/instance-id
+      ;;
+    /sbin/ip)
+      echo /sbin/ip -o addr
+      ;;
+    yum)
+      [ "$4" = "updateinfo" ] && echo "$@"
       ;;
     rpm)
       case "$2" in
@@ -82,22 +103,26 @@ verify_command() {
           ;;
       esac
       ;;
-    apt-cache)
-      echo "$@"
-      ;;
     docker)
       case "$2" in
         ps)
           echo "$@"
           ;;
         exec)
+          options="--user 0"
           container_id="$5"
           IFS=$'\t'
           set -- $(echo "$@" | xargs printf '%s\t')
           IFS=' '
           set -- $8
+          enviroment=$(verify_enviroment "$@")
+          if [ -n "$enviroment" ]
+          then
+            options="$options --env '$enviroment'"
+            shift
+          fi
           command=$(verify_command "$@")
-          [ -n "$command" ] && echo docker exec --user 0 $container_id $command
+          [ -n "$command" ] && echo docker exec $options $container_id $command
           ;;
       esac
       ;;
@@ -107,10 +132,23 @@ verify_command() {
 IFS=';'
 set -- $SSH_ORIGINAL_COMMAND
 
-stty cols 1000
+if test_stty "$1"
+then
+  $1
+else
+  exit 126
+fi
 
 IFS=' '
 set -- $(printf '%s' "$2" | escape)
+
+enviroment=$(verify_enviroment "$@")
+if [ -n "$enviroment" ]
+then
+  export "$enviroment"
+  shift
+fi
+
 set -- $(verify_command "$@")
 
 case "$1" in
